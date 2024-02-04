77e851bb463f046ef1e9b56795585e8fc37c877c
Andrey <79798383+seoseo29@users.noreply.github.com>
2024-02-04 14:55:36 +0300

    Update start_tonhub.sh

--- start_tonhub.sh
+++ start_tonhub.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 npm install
 
+if [ -z "$1" ]
+then
+  echo "Please specify the number of GPUs as an argument."
+  exit 1
+fi
 
 while true; do
-  node send_universal.js --api tonhub --bin ./pow-miner-cuda
-  sleep 1;
-done;
+  for i in $(seq 0 $(($1 - 1)))
+  do
+      node send_universal.js --api lite --bin ./pow-miner-cuda --givers 1000 --gpu $i &
+  done
+  sleep 1
+  wait
+done
+
