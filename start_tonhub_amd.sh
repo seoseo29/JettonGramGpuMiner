26a721a0f6f002172631766483ac4c618bc171a0
Andrey <79798383+seoseo29@users.noreply.github.com>
2024-02-04 14:56:46 +0300

    Update start_tonhub_amd.sh

--- start_tonhub_amd.sh
+++ start_tonhub_amd.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 npm install
 
+if [ -z "$1" ]
+then
+  echo "Please specify the number of GPUs as an argument."
+  exit 1
+fi
 
 while true; do
-  node send_universal.js --api tonhub --bin ./pow-miner-opencl
-  sleep 1;
-done;
+  for i in $(seq 0 $(($1 - 1)))
+  do
+      node send_universal.js --api lite --bin ./pow-miner-opencl --givers 1000 --gpu $i &
+  done
+  sleep 1
+  wait
+done
