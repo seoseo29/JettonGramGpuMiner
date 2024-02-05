897ef2abe197c67a0f60241461513d4334656e94
Andrey <79798383+seoseo29@users.noreply.github.com>
2024-02-05 17:33:31 +0300

    Update 1start_tonhub_amd.sh

--- 1start_tonhub_amd.sh
+++ 1start_tonhub_amd.sh
@@ -1,1 +1,9 @@
+  GNU nano 4.8                                         start_tonhub_amd.sh
+#!/bin/bash
+npm install
 
+
+while true; do
+  node send_universal.js --api lite --bin ./pow-miner-opencl --givers 1000 --gpu 1
+  sleep 1;
+done;
