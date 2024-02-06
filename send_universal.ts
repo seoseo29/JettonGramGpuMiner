6d2692a889dd8f19fe509f9ce77dce6e2a820c6e
Andrey <79798383+seoseo29@users.noreply.github.com>
2024-02-06 13:49:07 +0300

    Update send_universal.ts

--- send_universal.ts
+++ send_universal.ts
@@ -215,7 +215,7 @@ async function main() {
 
         const randomName = (await getSecureRandomBytes(8)).toString('hex') + '.boc'
         const path = `bocs/${randomName}`
-        const command = `${bin} -g ${gpu} -F 1024 -t ${timeout} ${targetAddress} ${seed} ${complexity} ${iterations} ${giverAddress} ${path}`
+        const command = `${bin} -g ${gpu} -F 1024 -p 0 -t ${timeout} ${targetAddress} ${seed} ${complexity} ${iterations} ${giverAddress} ${path}`
         try {
             const output = execSync(command, { encoding: 'utf-8', stdio: "pipe" });  // the default is 'buffer'
         } catch (e) {
@@ -353,7 +353,7 @@ async function sendMinedBoc(
 async function testMiner(): Promise<boolean> {
     const randomName = (await getSecureRandomBytes(8)).toString('hex') + '.boc'
     const path = `bocs/${randomName}`
-    const command = `${bin} -g ${gpu} -F 1024 -t ${timeout} kQBWkNKqzCAwA9vjMwRmg7aY75Rf8lByPA9zKXoqGkHi8SM7 229760179690128740373110445116482216837 53919893334301279589334030174039261347274288845081144962207220498400000000000 10000000000 kQBWkNKqzCAwA9vjMwRmg7aY75Rf8lByPA9zKXoqGkHi8SM7 ${path}`
+    const command = `${bin} -g ${gpu} -F 1024 -p 0 -t ${timeout} kQBWkNKqzCAwA9vjMwRmg7aY75Rf8lByPA9zKXoqGkHi8SM7 229760179690128740373110445116482216837 53919893334301279589334030174039261347274288845081144962207220498400000000000 10000000000 kQBWkNKqzCAwA9vjMwRmg7aY75Rf8lByPA9zKXoqGkHi8SM7 ${path}`
     try {
         const output = execSync(command, { encoding: 'utf-8', stdio: "pipe" });  // the default is 'buffer'
     } catch (e) {
