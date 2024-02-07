75209d577ce3f9825156c84d9e4ca2856289c705
Andrey <79798383+seoseo29@users.noreply.github.com>
2024-02-07 18:03:52 +0300

    Update start_tonhub.sh

--- start_tonhub.sh
+++ start_tonhub.sh
@@ -10,7 +10,7 @@ fi
 while true; do
   for i in $(seq 0 $(($1 - 1)))
   do
-      node send_universal.js --api tonapi --bin ./pow-miner-cuda --givers tonhub --gpu $i &
+      node send_universal.js --api lite --bin ./pow-miner-cuda --givers tonhub --gpu $i &
   done
   sleep 1
   wait
