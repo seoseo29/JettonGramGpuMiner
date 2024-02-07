e6c1cf4c78ca1a196dd9dfea5aad78306607a014
Andrey <79798383+seoseo29@users.noreply.github.com>
2024-02-07 13:23:16 +0300

    Update givers.js

--- givers.js
+++ givers.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.givers1000 = exports.givers100 = void 0;
 exports.givers100 = [
-    { address: 'EQBoATvbIa9vA7y8EUQE4tlsrrt0EhSUK4mndp49V0z7Me3M', reward: 100 },
+    { address: 'EQAFaPmLLhXveHcw3AYIGDlHbGAbfQWlH45WGf4K4D6DNZxY', reward: 100 },
 ];
 exports.givers1000 = [
     { address: 'EQAvheS_G-U57CE55UlwF-3M-cc4cljbLireYCmAMe_RHWGF', reward: 1000 },
